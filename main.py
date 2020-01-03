c7f7ddabaab947d8f481571107ebd6d3f4bb9505
shrey1098 <54493774+shrey1098@users.noreply.github.com>
2020-01-03 20:52:25 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ from BSHDEV.wsgi import application as app
 import os
 
 def env_vars(request):
-    return os.environ.get('TEXT_ANALYTICS_SUBSCRIPTION_KEY', 'Specified environment variable is not set.')
+    return os.environ.get('TEXT_ANALYTICS_SUBSCRIPTION_KEY')
   
